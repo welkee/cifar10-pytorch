fc45ea2e8f10232670e4fbd701b15e15bb515763
welkee <130264876+welkee@users.noreply.github.com>
2024-09-25 00:57:01 +0800

    Add files via upload

---
+++ para1.py
@@ -0,0 +1,61 @@
+import torch
+from torchsummary import summary
+from thop import profile
+
+# 假设你的模型在 'nets' 文件夹中
+def load_model(model_name):
+    if model_name == 'VGG16':
+        from nets.VGG import VGG
+        return VGG('VGG16')
+    elif model_name == 'VGG19':
+        from nets.VGG import VGG
+        return VGG('VGG19')
+    elif model_name == 'ResNet18':
+        from nets.ResNet import ResNet18
+        return ResNet18()
+    elif model_name == 'ResNet34':
+        from nets.ResNet import ResNet34
+        return ResNet34()
+    elif model_name == 'LeNet5':
+        from nets.LeNet5 import LeNet5
+        return LeNet5()
+    elif model_name == 'AlexNet':
+        from nets.AlexNet import AlexNet
+        return AlexNet()
+    elif model_name == 'DenseNet':
+        from nets.DenseNet import densenet_cifar
+        return densenet_cifar()
+    elif model_name == 'MobileNetv1':
+        from nets.MobileNetv1 import MobileNet
+        return MobileNet()
+    elif model_name == 'MobileNetv2':
+        from nets.MobileNetv2 import MobileNetV2
+        return MobileNetV2()
+    else:
+        raise ValueError(f"Unknown model name: {model_name}")
+
+if __name__ == '__main__':
+    # 设定模型名称
+    model_name = 'ResNet34'  # 你可以根据需要更改这个名称
+
+    # 创建模型
+    model = load_model(model_name)
+    # 创建 VGG16 模型和输入数据
+
+    input_data = torch.randn(1, 3, 32, 32)  # 假设输入为 1 张 224x224 的 RGB 图像
+
+    # 计算 FLOPs
+    flops, params = profile(model, inputs=(input_data,))
+    print(f'FLOPs: {flops}, Parameters: {params}')
+    Flops_txt = "Flops.txt"
+    output =(f'FLOPs: {flops}, Parameters: {params}')
+    with open(Flops_txt, "a+") as f:
+        f.write( output+ '\n')
+        f.close
+    # 选择设备
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = model.to(device)
+
+    # 打印模型的参数数量和 FLOPS
+    print("Model Summary:")
+    summary(model, (3, 32, 32))  # CIFAR-10 图像的输入尺寸
